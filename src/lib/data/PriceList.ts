9f2e887e9f60c264f2d38ad7863df0b372caec6b
Marek <zacikmarek@gmail.com>
2024-04-19 11:14:29 +0200

    #42 Removed "Other" category prom price list

--- src/lib/data/PriceList.ts
+++ src/lib/data/PriceList.ts
@@ -24,14 +24,14 @@ export const PriceList: PriceGroup[] = [
 			{ name: 'Dodávky a SUV od 2t do 3t', price: '0,80€/km' },
 		]
 	},
-	{
-		title: 'Ostatné', 
-		items: [
-			{ name: 'Sťažené nakladanie', price: '20€/30min' },
-			{ name: 'Príplatok za sviatok/víkend/noc', price: '20€' },
-			{ name: 'Čakanie', price: '20€/hod*' },
-			{ name: 'Čistenie ložnej plochy (olej, pohonné hmoty)', price: '10€' },
-			{ name: 'Zrušený / márny výjazd ( po 10 min. od objednania )', price: '50€' },
-		]
-	},
+	// {
+	// 	title: 'Ostatné', 
+	// 	items: [
+	// 		{ name: 'Sťažené nakladanie', price: '20€/30min' },
+	// 		{ name: 'Príplatok za sviatok/víkend/noc', price: '20€' },
+	// 		{ name: 'Čakanie', price: '20€/hod*' },
+	// 		{ name: 'Čistenie ložnej plochy (olej, pohonné hmoty)', price: '10€' },
+	// 		{ name: 'Zrušený / márny výjazd ( po 10 min. od objednania )', price: '50€' },
+	// 	]
+	// },
 ]; 
